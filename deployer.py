980a3d5bf9f75ea42cce06e7176c8cb0dae3b384
Simon Lefort <simon.lefort@gmail.com>
2021-05-21 08:23:30 +0200

    feat(deployer): add run command to deployer

--- deployer.py
+++ deployer.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import click
+import os
 
 __author__ = "Simon Lefort"
+
+def local_exec(cmd):
+    """
+    Execute command locally
+    """
+    click.echo(cmd)
+    os.system(cmd)
 
 @click.group()
 def main():
@@ -9,6 +17,26 @@ def main():
     Simple CLI wrapper around my main deployment/development actions
     """
     pass
+
+@main.command()
+@click.option('--name','-n', default='', help='container name')
+@click.option('--build','-b', default='False', help='build before running')
+def run(name, build):
+    """
+    build/run the ci containers
+    --name -n name of the service to run
+    --build build the container
+    """
+    cmd = 'docker-compose rm -s -f ' + name
+    local_exec(cmd)
+    cmd = 'docker-compose up -d'
+    if build == 'True':
+        cmd = cmd + ' --build'
+    cmd = cmd + ' ' + name
+    local_exec(cmd)
+    # fix_ssh_target('ci_bootstrap_target')
+    # fix_ssh_target('ci_target')
+    return
 
 if __name__ == "__main__":
     main()
