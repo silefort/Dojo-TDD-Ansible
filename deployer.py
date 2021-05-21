46fd1e47f06cbaf04b8741f81f09f64ec2a7ec58
Simon Lefort <simon.lefort@gmail.com>
2021-05-21 08:15:29 +0200

    feat(deployer): add click and main to deployer

--- deployer.py
+++ deployer.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+import click
+
 __author__ = "Simon Lefort"
+
+@click.group()
+def main():
+    """
+    Simple CLI wrapper around my main deployment/development actions
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
